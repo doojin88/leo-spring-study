2eef653ed5e7666144175e709b9d632d3a2eb6ec
dev <hyeonseo@devui-MacBookPro.local>
2021-10-01 09:54:49 +0900

    update hyeonseo's repo

---
+++ hyeonseo/mvnhello/src/test/java/kr/study/mvnhello/MvnhelloApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.study.mvnhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnhelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
